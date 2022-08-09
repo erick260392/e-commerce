1504651981dc0616521f5b46e0203b5256861d79
erick260392 <erickliconamorales2016@gmail.com>
2022-08-09 08:32:17 -0500

    styles

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+
+const {register,handleSubmit}= useForm()
+
+const Submit = data => {
+    alert("hice sumit");
+}
+
     return (
-        <div>
+        <div className='container'>
             <h1>login</h1>
+            <Form onSubmit={handleSubmit(Submit)}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" {...register("email") } />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" {...register("password") } />
+      </Form.Group>
+    
+      <Button variant="primary" >
+        Submit
+      </Button>
+    </Form>
         </div>
     );
 };
